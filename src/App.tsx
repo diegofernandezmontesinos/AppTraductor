8c3b46fcb0d57d691ff9375570e7234b03bb6859
dfmontesinos <dfmontesinos@gmail.com>
2023-07-14 20:48:44 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+
+//iniciamos el estado de la app
+const initialState={
+  fromLanguage:'auto',
+  toLanguage:'en',
+  fromText:'',
+  result:'',
+  loding: false
+
+}
+
+ function reducir(state, action){
+  
+ }
+
+
+function App() {
+  
+  return (
+    <div className='App'>
+      <h1>DFM Translate</h1>
+    </div>
+  )
+}
+
+export default App
